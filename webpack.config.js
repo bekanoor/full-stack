1cd11ebd233b7ad7ff8f626165c2e4df98bcc236
My Name <passivevoice3@gmail.com>
2022-01-28 17:07:08 +0300

    set up project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
